85e2e98efd97a045132d2b49ee2e4c1faf989bc8
Jaxon Hermansen <jaxherm35@gmail.comgit config --user.name Jaxon>
2022-04-14 15:05:21 -0600

    problem 5

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -64,7 +64,18 @@ var score = 74
 */
 
 //Code Here
-
+if(score >= 90){
+  console.log('A')
+}if(score >= 80 && score <= 89){
+  console.log('B')
+}if(score >= 70 && score <= 79){
+  console.log('C')
+}if(score >= 60 && score <= 69){
+  console.log('D')
+  if(score > 0 && score <= 59){
+  }
+  console.log('F')
+  }
 
 ////////// Intermediate Problems //////////
 
